507f978a8ee94456b9196ec03dfefcf1650afb69
AndinaZS <alexa.markova@outlook.com>
2022-04-12 21:32:09 +0300

    les_3

--- 2.1-databases/models_list_displaying/books/views.py
+++ 2.1-databases/models_list_displaying/books/views.py
@@ -1,7 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from books.models import Book
+
+
+def index_view(request):
+    return redirect('books')
 
 
 def books_view(request):
     template = 'books/books_list.html'
-    context = {}
+    context = {'books': [book for book in Book.objects.all()]}
+    return render(request, template, context)
+
+
+def books_date_view(request, pub_date):
+    template = 'books/books_list.html'
+    try:
+        prev_data = Book.objects.order_by('pub_date').filter(pub_date__lt=pub_date).first().pub_date
+    except AttributeError:
+        prev_data = None
+    try:
+        next_data = Book.objects.order_by('pub_date').filter(pub_date__gt=pub_date).first().pub_date
+    except AttributeError:
+        next_data = None
+    context = {'books': [book for book in Book.objects.filter(pub_date=pub_date)],
+               'prev_data': prev_data,
+               'next_data': next_data
+               }
     return render(request, template, context)
--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render, redirect
 
+from phones.models import Phone
+
+SORTING_FIELD = {'name': 'name',
+                 'id': 'id',
+                 'min_price': 'price',
+                 'max_price': '-price'}
+
 
 def index(request):
     return redirect('catalog')
@@ -7,11 +14,12 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    field = request.GET.get('sort', 'id')
+    context = {'phones': [phone for phone in Phone.objects.all().order_by(SORTING_FIELD[field])]}
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    context = {'phone': Phone.objects.get(slug=slug)}
     return render(request, template, context)
