9aa42181284411f62fe49e1d43a01078c6f0209d
AndinaZS <alexa.markova@outlook.com>
2022-04-21 14:18:01 +0300

    les_4

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,29 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Article, Tag, Scope
 
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        count = 0
+        for form in self.forms:
+            count += form.cleaned_data.get('is_main', 0)
+        if count > 1:
+            raise ValidationError('Основным может быть только один раздел')
+        elif count == 0:
+            raise ValidationError('Укажите основной раздел')
+        return super().clean()
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    formset = ScopeInlineFormset
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['title', 'text', 'published_at', 'image']
+    inlines = [ScopeInline]
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name']
--- 2.2-databases-2/orm_migrations/school/models.py
+++ 2.2-databases-2/orm_migrations/school/models.py
@@ -1,6 +1,19 @@
 from django.db import models
 
 
+class Student(models.Model):
+    name = models.CharField(max_length=30, verbose_name='Имя')
+    teachers = models.ManyToManyField(Teacher, related_name='students')
+    group = models.CharField(max_length=10, verbose_name='Класс')
+
+    class Meta:
+        verbose_name = 'Ученик'
+        verbose_name_plural = 'Ученики'
+
+    def __str__(self):
+        return self.name
+
+
 class Teacher(models.Model):
     name = models.CharField(max_length=30, verbose_name='Имя')
     subject = models.CharField(max_length=10, verbose_name='Предмет')
@@ -8,19 +21,6 @@ class Teacher(models.Model):
     class Meta:
         verbose_name = 'Учитель'
         verbose_name_plural = 'Учителя'
-
-    def __str__(self):
-        return self.name
-
-
-class Student(models.Model):
-    name = models.CharField(max_length=30, verbose_name='Имя')
-    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
-    group = models.CharField(max_length=10, verbose_name='Класс')
-
-    class Meta:
-        verbose_name = 'Ученик'
-        verbose_name_plural = 'Ученики'
 
     def __str__(self):
         return self.name
