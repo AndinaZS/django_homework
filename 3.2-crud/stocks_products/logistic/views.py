ee4d98db88420e8bbf5cde51d69cfb786642cd34
AndinaZS <alexa.markova@outlook.com>
2022-05-07 13:07:41 +0300

    les_6

--- 3.2-crud/stocks_products/logistic/views.py
+++ 3.2-crud/stocks_products/logistic/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from rest_framework.viewsets import ModelViewSet
 
 from logistic.models import Product, Stock
@@ -5,12 +6,19 @@ from logistic.serializers import ProductSerializer, StockSerializer
 
 
 class ProductViewSet(ModelViewSet):
-    queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    # при необходимости добавьте параметры фильтрации
+    queryset = Product.objects.all()
+
+    def get_queryset(self):
+        title = self.request.GET.get('title', '')
+        description = self.request.GET.get('description', '')
+        queryset = Product.objects.all().filter(title__icontains=title, description__icontains=description)
+        return queryset.order_by('id')
 
 
 class StockViewSet(ModelViewSet):
-    queryset = Stock.objects.all()
+    queryset = Stock.objects.all().prefetch_related('positions').prefetch_related('products').order_by('id')
     serializer_class = StockSerializer
-    # при необходимости добавьте параметры фильтрации
+    search_fields = ['positions__product__title']
+    filter_fields = ['positions__product__id']
+
