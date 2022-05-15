047da961063c8b43e677b7a6ba6be697838c7153
AndinaZS <alexa.markova@outlook.com>
2022-05-15 10:32:33 +0300

    les_7_base

--- 3.3-permissions/api_with_restrictions/advertisements/filters.py
+++ 3.3-permissions/api_with_restrictions/advertisements/filters.py
@@ -1,12 +1,13 @@
-from django_filters import rest_framework as filters
+from django_filters import rest_framework as filters, DateFromToRangeFilter, AllValuesFilter
 
 from advertisements.models import Advertisement
 
 
 class AdvertisementFilter(filters.FilterSet):
-    """Фильтры для объявлений."""
-
-    # TODO: задайте требуемые фильтры
+    created_at = DateFromToRangeFilter()
+    status = AllValuesFilter(field_name='status')
 
     class Meta:
         model = Advertisement
+        fields = ['created_at', 'status']
+
